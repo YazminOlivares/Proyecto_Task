8e95534275ab92cf160423f35c66a4d36e3f218b
YazminOlivares <146601100+YazminOlivares@users.noreply.github.com>
2024-10-27 16:44:08 -0700

    Correcciones

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/tasks',taskRoutes);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,12 +5,12 @@ const taskController = require('../controllers/taskController')
 
 router.get('/', taskController.getAllTasks);
 
-router.post('/', taskController.createTask);
+router.post('/post', taskController.createTask);
 
 router.get('/:id', taskController.getTaskById);
 
-router.put('/:id', taskController.updateTask);
+router.put('/update/:id', taskController.updateTask);
 
-router.delete('/:id', taskController.deleteTask);
+router.delete('/delete/:id', taskController.deleteTask);
 
 module.exports = router;
